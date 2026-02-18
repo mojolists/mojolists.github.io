d31a21489a287087707968049e81e3ff3bb90af1
mojolists <dough@mojolists.com>
2026-02-18 15:24:18 -0600

    please god, let this work

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 module.exports = function(eleventyConfig) {
-    // Keeps your images where they belong
     eleventyConfig.addPassthroughCopy("assets/img");
 
-    // Automatically sorts reviews by date (newest first)
     eleventyConfig.addCollection("reviews", function(collectionApi) {
         return collectionApi.getFilteredByGlob("reviews/*.md").sort((a, b) => {
             return b.date - a.date;
