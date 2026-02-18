d23db131b0e93d921c2d97202058ee5273d31994
mojolists <dough@mojolists.com>
2026-02-18 15:46:33 -0600

    zz

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function(eleventyConfig) {
+    // This tells the engine where your images are
     eleventyConfig.addPassthroughCopy("assets/img");
 
+    // This creates the automated review list
     eleventyConfig.addCollection("reviews", function(collectionApi) {
         return collectionApi.getFilteredByGlob("reviews/*.md").sort((a, b) => {
-            return b.date - a.date;
+            return (b.date || 0) - (a.date || 0);
         });
     });
 
